31d78d793b2fade091c583e3ed38a18d24d05a2d
limanjohnson <liamjnell@gmail.com>
2025-02-04 21:29:05 -0700

    Homework Program

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using Homework;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // Math Assignment
+        MathAssignment mathAssignment1 = new MathAssignment("Liam Nell", "Algebra", "Algebra for dummies", "1 - 10");
+
+        // Writing Assignment
+        WritingAssignment writingAssignment1 =
+            new WritingAssignment("Rogan Nell", "Creative Writing", "The Aegis of Tron");
+        
+        // Log Math Homework
+        Console.WriteLine(mathAssignment1.GetSummary());
+        Console.WriteLine(mathAssignment1.GetHomeworkList());
+        
+        // Log Writing Homework
+        Console.WriteLine(writingAssignment1.GetSummary());
+        Console.WriteLine(writingAssignment1.GetWritingInformation());
+        
+        
+        
     }
+    
+    
 }
---
+++ week05/Homework/WritingAssignment.cs
@@ -0,0 +1,17 @@
+﻿namespace Homework;
+
+public class WritingAssignment : Assignment
+{
+    private string _title;
+
+    public WritingAssignment(string studentName, string topic, string title) : base(studentName, topic)
+    {
+        _title = title;
+    }
+
+    public string GetWritingInformation()
+    {
+        return $"Title: {_title} - {GetStudentName()}";
+    }
+    
+}
